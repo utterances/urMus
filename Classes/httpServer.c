bfa9e1924b388f8efb69d334a48af24394cb0225
Jong Wook Kim <jongwook@umich.edu>
2010-07-21 22:18:55 -0400

    added /getlog?since=lognumber webpage to retrieve DPrint history

--- Classes/httpServer.c
+++ Classes/httpServer.c
@@ -10,6 +10,7 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "mongoose.h"
+
 #include <pthread.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -271,6 +272,22 @@ eval_script(struct mg_connection *conn,
   }
 }
 
+extern char * ur_GetLog(int since);	// to avoid messing up with objective-c code
+
+static void
+get_log(struct mg_connection *conn,
+		const struct mg_request_info *request_info,
+		void *user_data) 
+{
+	char *query=mg_get_var(conn,"since");
+	int since=(query==NULL)?0:atoi(query);
+	char *log=ur_GetLog(since);	
+	mg_printf(conn, "HTTP/1.1 200 OK\r\n"
+			  "Content-Type: text/plain\r\n\r\n"
+			  "%s", log);
+	free(log);
+}
+
 void 
 http_start(const char *web_root_, const char *doc_root_) 
 {
@@ -287,6 +304,7 @@ http_start(const char *web_root_, const char *doc_root_)
   mg_set_uri_callback(ctx, "/open_file", &open_file, NULL);
   mg_set_uri_callback(ctx, "/upload_file", &upload_file, NULL);
   mg_set_uri_callback(ctx, "/upload_script", &upload_script, NULL);
+  mg_set_uri_callback(ctx, "/getlog",&get_log,NULL);
 }
 
 void 
